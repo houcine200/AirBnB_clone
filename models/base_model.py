fc60dbc19247b013c8c9ce7dbd9ce15a32c4e7a8
Houcine Walaq <houcine.walaq@gmai.com>
2023-11-06 20:45:33 +0100

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -3,10 +3,15 @@ from uuid import uuid4
 from datetime import datetime
 
 class BaseModel():
-    def __init__(self):
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                    if key != "__class__":
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return ("[{}] ({}) {}".format(self.__class__.__name__,
