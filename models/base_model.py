50da7653089058d16cf262ac84442f5c036a1e43
Houcine Walaq <houcine.walaq@gmai.com>
2023-11-07 17:28:26 +0100

    task 5

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from uuid import uuid4
 from datetime import datetime
+from models import storage
 
 
 class BaseModel():
@@ -17,6 +18,7 @@ class BaseModel():
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         return ("[{}] ({}) {}".format(self.__class__.__name__,
@@ -24,6 +26,7 @@ class BaseModel():
 
     def save(self):
         self.updated_at = datetime.now()
+        storage.save()
         
     def to_dict(self):                                                       
         my_dict = {}
---
+++ models/engine/file_storage.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import json
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return FileStorage.__objects
+    
+    def new(self, obj):
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        dict_obj = {}
+        for key, val in FileStorage.__objects.items():
+            dict_obj[key] = val.to_dict()
+        with open(FileStorage.__file_path, "w", encoding="utf_8") as jfile:
+            json.dump(dict_obj, jfile)
+
+    def reload(self):
+        try:
+            with open(FileStorage.__file_path, "r") as file:
+                dict_to_obj = json.load(file)
+            for key, val in dict_to_obj.items():
+                    FileStorage.__objects[key] = eval(val["__class__"] + '(**val)')
+        except FileNotFoundError:
+            pass
+    
