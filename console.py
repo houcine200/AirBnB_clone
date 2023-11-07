97869dc5540e781f6778ee95d57b87be0a254460
Elimali hamza <elimal.hamza@gmail.com>
2023-11-07 10:47:58 -0800

    task 6

---
+++ console.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb)"
+
+    def do_quit(self, arg):
+        return True
+
+    def do_EOF(self, arg):
+        print()
+        return True
+
+    def emptyline(self):
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
