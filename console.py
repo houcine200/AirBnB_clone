a7f0a8e750f38eb4fd3481fcb34598e22345acf2
Houcine Walaq <houcine.walaq@gmai.com>
2023-11-09 20:49:50 +0100

    task 10

--- console.py
+++ console.py
@@ -2,11 +2,26 @@
 
 import cmd
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 from models import storage
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb)"
-    class_names = {"BaseModel": BaseModel} 
+
+    class_names = {
+        "BaseModel": BaseModel,
+        "User": User,
+        "State": State,
+        "City": City,
+        "Amenity": Amenity,
+        "Place": Place,
+        "Review": Review,     
+                   } 
     def do_quit(self, arg):
         return True
 
