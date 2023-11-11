eef5200d53335751d53ddbb78f86ccecc7384d81
Houcine Walaq <houcine.walaq@gmai.com>
2023-11-11 13:02:11 +0100

    base_model.py documentation

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,29 @@ from datetime import datetime
 
 
 class BaseModel():
+    """
+    BaseModel class.
+
+    It provides ID generation, timestamping, string representation,
+    saving to storage, and conversion to dictionary.
+
+    Attributes:
+    - id (str): Unique identifier for each instance.
+    - created_at (datetime): Timestamp indicating the creation time.
+    - updated_at (datetime): Timestamp indicating the last update time.
+    """
     def __init__(self, *args, **kwargs):
+        """
+        Initialize a new instance of BaseModel.
+
+        If provided with kwargs, populate instance attributes from kwargs.
+        Otherwise, generate a new ID, set creation and update timestamps,
+        and add the instance to storage.
+
+        Args:
+        - *args: Variable length argument list.
+        - **kwargs: Arbitrary keyword arguments.
+        """
         from models import storage
         if kwargs:
             for key, value in kwargs.items():
@@ -24,11 +46,20 @@ class BaseModel():
                                       self.id, self.__dict__))
 
     def save(self):
+        """
+        Save the instance to storage and update the 'updated_at' timestamp.
+        """
         from models import storage
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
+        """
+        Convert the instance to a dictionary.
+
+        Returns:
+        - dict: Dictionary representation of the instance.
+        """
         from models import storage
         my_dict = {}
         my_dict["__class__"] = self.__class__.__name__
