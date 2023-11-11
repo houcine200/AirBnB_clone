a2fd2db7916c4569e61773bf61246ef3f995177d
Houcine Walaq <houcine.walaq@gmai.com>
2023-11-11 20:07:33 +0100

    pycodestyle for test_base_model x4

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,6 +3,7 @@ import unittest
 from datetime import datetime
 from models.base_model import BaseModel
 
+
 class TestBaseModel(unittest.TestCase):
     def test_creation_with_kwargs(self):
         kwargs = {
@@ -14,9 +15,15 @@ class TestBaseModel(unittest.TestCase):
         instance = BaseModel(**kwargs)
 
         self.assertEqual(instance.id, "test_id")
-        self.assertEqual(instance.created_at, datetime.fromisoformat("2023-01-01T12:00:00"))
-        self.assertEqual(instance.updated_at, datetime.fromisoformat("2023-01-02T14:30:00"))
-        self.assertEqual(getattr(instance, "custom_attribute", None), "some_value")
+        self.assertEqual(
+            instance.created_at, datetime.fromisoformat("2023-01-01T12:00:00")
+            )
+        self.assertEqual(
+            instance.updated_at, datetime.fromisoformat("2023-01-02T14:30:00")
+            )
+        self.assertEqual(
+            getattr(instance, "custom_attribute", None), "some_value"
+            )
 
     def test_creation_without_kwargs(self):
         instance = BaseModel()
@@ -35,13 +42,17 @@ class TestBaseModel(unittest.TestCase):
         self.assertNotEqual(instance.updated_at, original_updated_at)
 
     def test_to_dict_conversion(self):
-        instance = BaseModel(id="test_id", created_at="2023-01-01T12:00:00", custom_attribute="some_value")
+        instance = BaseModel(
+            id="test_id", created_at="2023-01-01T12:00:00",
+            custom_attribute="some_value"
+            )
         instance_dict = instance.to_dict()
 
         self.assertEqual(instance_dict["__class__"], "BaseModel")
         self.assertEqual(instance_dict["id"], "test_id")
         self.assertEqual(instance_dict["created_at"], "2023-01-01T12:00:00")
         self.assertEqual(instance_dict.get("custom_attribute"), "some_value")
+
 
 if __name__ == '__main__':
     unittest.main()
