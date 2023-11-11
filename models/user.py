dbae9696c18e8edc63c0517ca34ae3702b7b0a88
Houcine Walaq <houcine.walaq@gmai.com>
2023-11-11 18:48:52 +0100

    documentation for user.py

--- models/user.py
+++ models/user.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
+"""
+User class that inherits from BaseModel
+"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
+    """
+    define User classe that
+    inherits from BaseModel class
+    """
     email = ""
     password = ""
     first_name = ""
