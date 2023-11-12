46f887c36ae5abe210854e3aa3686feffc580e86
Houcine Walaq <houcine.walaq@gmai.com>
2023-11-12 15:59:21 +0100

    city tokyo unit test

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -4,27 +4,25 @@ import unittest
 from models.city import City
 from models.state import State
 
-
-class TestAmenity(unittest.TestCase):
+class TestCity(unittest.TestCase):
 
     def setUp(self):
-        self.new_user = City()
+        self.new_city = City()
         self.s_id = State()
 
     def tearDown(self):
-        del self.new_user
+        del self.new_city
 
-    def test_user_type(self):
-        self.assertEqual(self.new_user.state_id, "")
-        self.assertEqual(self.new_user.name, "")
+    def test_city_attributes(self):
+        self.assertEqual(self.new_city.state_id, "")
+        self.assertEqual(self.new_city.name, "")
 
-    def test_user_attribute(self):
-        self.new_user.name = "Root"
-        self.new_user.state_id = self.s_id.id
+    def test_city_attribute_assignment(self):
+        self.new_city.name = "Tokyo"
+        self.new_city.state_id = self.s_id.id
 
-        self.assertEqual(self.new_user.name, "Root")
-        self.assertEqual(self.new_user.state_id, self.s_id.id)
-
+        self.assertEqual(self.new_city.name, "Tokyo")
+        self.assertEqual(self.new_city.state_id, self.s_id.id)
 
 if __name__ == '__main__':
     unittest.main()
