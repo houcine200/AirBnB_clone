a1b1fd08c6a7dc16da975642806fb06c1856972a
Elimali hamza <elimal.hamza@gmail.com>
2023-11-12 06:23:55 -0800

    test_base

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
+"""
+Unit tests for the BaseModel class in the models module.
+"""
 import unittest
 from datetime import datetime
 from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
+    """
+    Test cases for the BaseModel class.
+    """
+
     def test_creation_with_kwargs(self):
+        """
+        Test case for creating a BaseModel instance with keyword arguments.
+        """
         kwargs = {
             "id": "test_id",
             "created_at": "2023-01-01T12:00:00",
@@ -26,6 +36,9 @@ class TestBaseModel(unittest.TestCase):
             )
 
     def test_creation_without_kwargs(self):
+        """
+        Test case for creating a BaseModel instance without keyword arguments.
+        """
         instance = BaseModel()
 
         self.assertIsNotNone(instance.id)
@@ -33,6 +46,9 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(instance.updated_at, datetime)
 
     def test_save_updates_updated_at(self):
+        """
+        Test case to ensure the save method updates the 'updated_at' attribute.
+        """
         instance = BaseModel()
         original_updated_at = instance.updated_at
 
@@ -42,6 +58,9 @@ class TestBaseModel(unittest.TestCase):
         self.assertNotEqual(instance.updated_at, original_updated_at)
 
     def test_to_dict_conversion(self):
+        """
+        Test case for converting a BaseModel instance to a dictionary.
+        """
         instance = BaseModel(
             id="test_id", created_at="2023-01-01T12:00:00",
             custom_attribute="some_value"
